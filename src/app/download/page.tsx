81d18439cbdbd247bd9c59b7b73ff8f61e0fd3e3
belonwu ronald <gronaldchia@gmail.com>
2025-04-10 01:28:31 +0100

    started building for download page, created the routes for the downloads

---
+++ src/app/download/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from '@/src/shared/NavBar/NavBar'
+import React from 'react'
+
+function downloadPage() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default downloadPage
