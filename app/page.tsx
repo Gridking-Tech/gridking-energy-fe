0e968f3e6b3c3e31b228b55fab7091b8a55c83cd
olu-martins <mayowasamuel86@gmail.com>
2025-05-14 22:03:43 +0100

    app router and product detail page with dumy data

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
 "use client";
+import { AnimatePresence, motion } from "framer-motion";
 import About from "../shared/About";
-import Carousel from "../shared/Carousel/Carousel";
+import HeroSection from "../components/HeroSection";
 import ProductShowcase from "../shared/HotSell";
 import NewArrivals from "../shared/NewArrivals";
 import NewsShowcase from "../shared/NewShowcase";
 import TalkExpert from "../shared/ServiceInquiry";
 import Footer from "../shared/Footer";
 import LoadCalculator from "../shared/LoadCalculator";
+import NavBar from "../shared/NavBar/NavBar";
+import DesktopHeader from "@/shared/Header";
+
 
 export default function Homepage() {
   return (
-    <div className="  w-screen h-screen">
-      <Carousel />
+    <div className="w-screen h-screen">
+      <HeroSection />
       <About />
       <NewArrivals />
-      <ProductShowcase />
+      {/* <ProductShowcase /> */}
       <TalkExpert />
       <LoadCalculator />
-      <NewsShowcase />
+      {/* <NewsShowcase /> */}
       <Footer />
     </div>
   );
--- app/products/[title]/page.tsx
+++ app/products/[title]/page.tsx
@@ -1,38 +1,26 @@
-'use client'
+"use client";
 import { homePageApi, productsApi } from "../../../api";
-import ProductDetails from "../../../shared/util/ProductDetails";
+import ProductDetail from "@/components/ProductDetail";
+import Footer from "@/shared/Footer";
+import DesktopHeader from "@/shared/Header";
 import { useParams } from "next/navigation";
 import { FadeLoader } from "react-spinners";
+
+function retrieveIdFromTitle(titleStr: string) {
+  const match = titleStr.match(/(\d+)$/);
+  return match ? parseInt(match[1], 10) : null;
+}
 
 const ProductsDescription = () => {
   const { title } = useParams<any>();
 
-  const {
-    data: productsData,
-    isLoading,
-    error,
-  } = productsApi.useGetProducts() as {
-    data: { products: any[] };
-    isLoading: boolean;
-    error: any;
-  };
-
-   const { data: ImageData } = homePageApi.useGetCarouselById("67ec910d2d2e858db2b1ca2a") as {
-      data: any;
-      isLoading: boolean;
-      error: any;
-    }
-
-  if (isLoading) return (
-    <div className="text-center h-screen w-scren flex justify-center items-center  font-black mt-10">
-        <FadeLoader height={15} />
-      </div>
-  );
-  if (error) return <div className="text-center mt-10">Error fetching product</div>;
-
+  const id = retrieveIdFromTitle(title);
   return (
     <div>
-      <ProductDetails productTitle={title} ImageData={ImageData} productsData={productsData.products} />
+      <DesktopHeader />
+
+      <ProductDetail />
+      <Footer />
     </div>
   );
 };
