c381614a7792314f71771f0c6c2c27c4c5aad989
olu-martins <mayowasamuel86@gmail.com>
2025-05-14 22:05:16 +0100

    light and dark mode theme toggle

---
+++ components/HeroSection/CarouselController.tsx
@@ -0,0 +1,21 @@
+const CarouselControls = ({ currentIndex, setCurrentIndex, images }: any) => {
+  return (
+    <div className="absolute bottom-4 z-50 left-1/2 transform -translate-x-1/2 flex gap-3">
+      {images?.map((_: any, index: any) => (
+        <div
+          key={index}
+          onClick={() => setCurrentIndex(index)}
+          onMouseOver={() => setCurrentIndex(index)}
+          className="focus:outline-none "
+        >
+          <div
+            className={`cursor-pointer transition-all w-[3.8rem] h-[2.1px] rounded-[20rem] duration-500 
+              ${index === currentIndex ? 'bg-white' : 'bg-gray-300 hover:bg-white'}`}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CarouselControls;
---
+++ components/ThemeToggle/index.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useTheme } from '@/app/context/ThemeContext';
+
+export default function ThemeToggle() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className=" rounded-full h-10 w-10  cursor-pointer  hover:bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200"
+      aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
+    >
+      {theme === 'light' ? '🌙' : '☀️'}
+    </button>
+  );
+}
