87fe37cc27f3985c4d37648544c604a46e87f56b
olu-martins <mayowasamuel86@gmail.com>
2025-05-17 23:12:36 +0100

    typed values

--- types/hotsell-type.ts
+++ types/hotsell-type.ts
@@ -5,6 +5,15 @@ export type categoryType = {
 export interface IProduct {
   name: string;
   id: string;
-  primaryImage: { url: string };
   category: categoryType;
+  status:string;
+  hotSell: boolean;
+  _id:string;
+  slug:string;
+  primaryImage: {
+    publicId:string;
+    url:string
+  };
+  updatedAt?:string
+  attributes:  any[]
 }
