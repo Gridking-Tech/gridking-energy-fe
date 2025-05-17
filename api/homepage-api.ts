4b00b07410a9d85615ab4327f6c2c5e875bc2eee
olu-martins <mayowasamuel86@gmail.com>
2025-05-17 23:13:04 +0100

    update homepage

--- api/homepage-api.ts
+++ api/homepage-api.ts
@@ -4,9 +4,9 @@ const api = new ApiSchema(process.env.NEXT_PUBLIC_APP_BASE_URL || "");
 
 export const homePageApi = {
   useGetHomePageResource: () =>
-    api.useFetchRequest("HOME_PAGE_RESOURCE", "/api/landing"),
+    api.useFetchRequest("HOME_PAGE_RESOURCE", "/landing"),
   useGetcarousel: () =>
-    api.useFetchRequest("CAROUSEL_RESOURCE", "/api/carousel"),
+    api.useFetchRequest("CAROUSEL_RESOURCE", "/carousel"),
   useGetCarouselById: (id: string) =>
-    api.useFetchRequest("CAROUSEL_SINGLE_RESOURCE", `/api/image/Carousel/${id}`),
+    api.useFetchRequest("CAROUSEL_SINGLE_RESOURCE", `/image/Carousel/${id}`),
 };
