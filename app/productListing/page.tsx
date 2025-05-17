1d3465fe10591f33474bfe306c987d4ed48738a9
olu-martins <mayowasamuel86@gmail.com>
2025-05-17 23:13:37 +0100

    update product categories

---
+++ app/productListing/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Banner from "@/components/Banner";
+import ProductLists from "@/components/ProductListing";
+
+const CategoriesPage = () => {
+  return (
+    <div>
+      <Banner />
+      <ProductLists />
+    </div>
+  );
+};
