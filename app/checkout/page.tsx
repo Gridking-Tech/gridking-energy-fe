62c68fd87b0bc9a5cb1da30a32dd01dbd7e8c34b
olu-martins <mayowasamuel86@gmail.com>
2025-05-21 21:03:55 +0100

    update checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,8 +1,14 @@
-
-
-const Checkout =()=>{
-    return(
-        <>checkout</>
-    )
-}
+"use client";
+import Pricing from "@/components/Pricing";
+import Footer from "@/shared/Footer";
+import DesktopHeader from "@/shared/Header";
+const Checkout = () => {
+  return (
+    <>
+      <DesktopHeader />
+      <Pricing />
+      <Footer />
+    </>
+  );
+};
 export default Checkout;
