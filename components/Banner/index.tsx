09685933dbc52a24bc630ca8890741af1412cf8a
olu-martins <mayowasamuel86@gmail.com>
2025-05-21 17:20:53 +0100

    add innovation banner

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -4,21 +4,28 @@ const Banner = () => {
   const slides = [
     {
       id: 1,
-      text1: "New",
-      text2: "Arrivals",
-      dotColor: "bg-pink-500",
-    },
-    {
-      id: 2,
       text1: "Power Your Future",
       text2: "with Clean Energy",
-      dotColor: "bg-orange-500",
     },
     {
       id: 2,
       text1: "Built on Trust,",
       text2: "Powered by Innovation",
-      dotColor: "bg-orange-500",
+    },
+    {
+      id: 3,
+      text1: "Harness the Sun,",
+      text2: "Empower Your Home",
+    },
+    {
+      id: 4,
+      text1: "Bright Today,",
+      text2: "Thriving Tomorrow",
+    },
+    {
+      id: 5,
+      text1: "Solar Strength,",
+      text2: "Sustainable Future",
     },
   ];
 
@@ -30,30 +37,28 @@ const Banner = () => {
     }, 2000);
     return () => clearInterval(interval);
   }, [slides.length]);
-
   return (
     <div className="relative w-full bg-black text-white overflow-hidden">
-      <div className="flex items-center justify-start h-24 px-4">
-        <span className="w-1 h-12 bg-gray-300 mr-4"></span>
+      <div className="flex items-center justify-start h-40 md:h-96 px-4">
+        <span className="w-1 h-20 md:h-32 bg-gray-300 mr-4"></span>
         <div className="flex items-center space-x-2">
-          <h1 className="text-3xl font-bold">
+          <h1 className="text-4xl md:text-5xl font-bold md:w-[70%]">
             {slides[currentSlide].text1}{" "}
-            <span className="text-orange-500">{slides[currentSlide].text2}</span>
+            <span className="text-orange-500">
+              {slides[currentSlide].text2}
+            </span>
           </h1>
-          <span
-            className={`w-2 h-2 rounded-full ${slides[currentSlide].dotColor} ml-2`}
-          ></span>
         </div>
       </div>
-      <div className="flex justify-center items-center h-8">
-        <span className="w-12 h-px bg-gray-400"></span>
+      <div className="flex justify-center items-center h-12">
+        <span className="w-16 h-px bg-gray-400"></span>
       </div>
-      <div className="flex justify-center space-x-2 py-2">
+      <div className="flex justify-center space-x-3 py-4">
         {slides.map((slide, index) => (
           <span
-            key={slide.id}
-            className={`w-2 h-2 rounded-full ${
-              currentSlide === index ? slide.dotColor : "bg-gray-400"
+            key={slide.id + "-" + index}
+            className={`w-3 h-3 rounded-full ${
+              currentSlide === index ? "bg-orange-500" : "bg-gray-400"
             }`}
           ></span>
         ))}
