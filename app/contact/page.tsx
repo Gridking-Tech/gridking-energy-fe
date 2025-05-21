d2e2dec618f0ca527de9fad27046cccb7ba2f7d5
olu-martins <mayowasamuel86@gmail.com>
2025-05-21 17:16:27 +0100

    add about and contact us page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,19 +1,10 @@
 "use client";
-import { homePageApi } from "../../api";
+import DesktopHeader from "@/shared/Header";
 import Footer from "../../shared/Footer";
-import NavBar from "../../shared/NavBar/NavBar";
-import ImagePlaceholder from "../../shared/Placeholders/ImagePlaceholder";
-import Image from "next/image";
 import React from "react";
-import {
-  FaCogs,
-  FaEnvelope,
-  FaLock,
-  FaPhone,
-  FaUsers,
-  FaWhatsapp,
-} from "react-icons/fa";
+import { FaEnvelope, FaWhatsapp } from "react-icons/fa";
 import { IoLocationSharp } from "react-icons/io5";
+import Banner from "@/components/Banner";
 
 const features = [
   {
@@ -45,53 +36,22 @@ const features = [
 ];
 
 export default function CustomSection() {
-  const { data: ImageId, isLoading } = homePageApi.useGetCarouselById(
-    "67ec910d2d2e858db2b1ca2a"
-  ) as {
-    data: any;
-    isLoading: boolean;
-    error: any;
-  };
-
   return (
-    <div className="w-screen   space-y-24">
-      <div className="absolute top-0 left-0 w-full z-50">
-        <NavBar />
-      </div>
-      <div className="w-full">
-        <div className="w-full">
-          {ImageId?.[0]?.url.length > 0 ? (
-            <div className="relative w-full h-[30rem]">
-              <Image
-                src={ImageId?.[0]?.url}
-                alt="Banner"
-                style={{ objectFit: "cover" }}
-                fill
-                className="absolute w-full h-full"
-              />
-              <div className="absolute inset-0 bg-black/30"></div>
-            </div>
-          ) : (
-            <ImagePlaceholder width={"100%"} height={"100%"} />
-          )}
-        </div>
-        <div className="w-full flex pl-20 font-black text-gray-600 justify-center flex-col  h-[3.5rem] bg-gray-200">
-          {"HOME > CONTACT US"}
-        </div>
-      </div>
-      {/* WHY CHOOSE US */}
-      <section className="text-center h-full md:h-[30rem] justify-center flex flex-col items-center ">
-        <div className="grid grid-cols-1  md:grid-cols-3 gap-8 item">
+    <div className="w-screen">
+      <DesktopHeader />
+      <Banner />
+      <section className="mx-auto w-full max-w-4xl px-6 py-12 md:py-36">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 place-items-center">
           {features.map((feature, index) => (
             <div
               key={index}
-              className="bg-white px-8 xl:py-16 rounded-xl border-2 shadow-lg hover:shadow-xl  transition text-center"
+              className="bg-white p-6 rounded-xl border-2 shadow-lg hover:shadow-xl transition-all text-center w-full max-w-sm h-72 flex flex-col justify-center items-center"
             >
               <div className="mb-4 flex justify-center">{feature.icon}</div>
-              <h3 className="text-2xl font-black mb-2 text-gray-900">
+              <h3 className="text-xl font-black mb-2 text-gray-900">
                 {feature.title}
               </h3>
-              <p className="text-black text-xl font-bold ">
+              <p className="text-black text-sm font-bold">
                 {feature.description}
               </p>
             </div>
