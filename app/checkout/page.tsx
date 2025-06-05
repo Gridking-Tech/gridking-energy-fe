4a9b79b4af00e5007353b57dae23e790ca52a183
Olumayowa <olumayowa@192.168.1.4>
2025-06-05 09:48:14 +0100

    update the header for checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Pricing from "@/components/Pricing";
+import DesktopHeader from "@/shared/Header";
 const Checkout = () => {
   return (
     <>
+      <DesktopHeader />
       <Pricing />
     </>
   );
